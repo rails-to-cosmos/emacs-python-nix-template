0442ee371ca9e97afbe031cf5d86cab1f4820394
Dmitry Akatov <dmitry.akatov@protonmail.com>
2024-04-15 18:49:09 +0300

    do not change ld path

--- devenv.nix
+++ devenv.nix
@@ -1,7 +1,7 @@
 { pkgs, ... }:
 
 {
-  env.LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath [pkgs.stdenv.cc.cc.lib];
+  # env.LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath [pkgs.stdenv.cc.cc.lib];
 
   packages = with pkgs; [
     python312Full
