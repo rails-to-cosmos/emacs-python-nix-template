4b04b3969a1c72e9a08585f5deb22b9b1b1a9638
Dmitry Akatov <dmitry.akatov@protonmail.com>
2024-04-23 13:14:02 +0300

    up nix

--- devenv.nix
+++ devenv.nix
@@ -13,25 +13,17 @@
   ];
 
   enterShell = ''
-    virtualenv .venv
     source $DEVENV_ROOT/.venv/bin/activate
   '';
 
-  scripts.wake.exec = ''
+  scripts.init.exec = ''
+    virtualenv .venv
     pip install poetry
     poetry install --with dev
+    wake
   '';
 
-  scripts.ruff.exec = "poetry run ruff $@";
-  scripts.ruff-lsp.exec = "poetry run ruff-lsp $@";
-  scripts.semgrep.exec = "poetry run semgrep $@";
-  scripts.pylsp.exec = "poetry run pylsp $@";
-  scripts.pyls.exec = "poetry run pylsp $@";
-  scripts.mypy.exec = "poetry run mypy $@";
-  scripts.mypy3.exec = "poetry run mypy $@";
-  scripts.pycodestyle.exec = "poetry run pycodestyle $@";
-  scripts.pyflakes.exec = "poetry run pyflakes $@";
-  scripts.flake8.exec = "poetry run flake8 $@";
-  scripts.pyright.exec = "poetry run pyright $@";
-  scripts.pylint.exec = "poetry run pylint $@";
+  scripts.wake.exec = ''
+
+  '';
 }
